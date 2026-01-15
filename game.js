39aee2051c052a66ad7c9e9de357f0906f5a4358
ariffrahmnn <ariffrahmnn1@gmail.com>
2026-01-15 16:23:32 +0700

    Fix game flow and added button start

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ var level = 0;
 var started = false;
 var onSound = true;
 
-$(document).on("keypress", function(){
+$("#start-btn").on("click", function(){
     
     if(!started) {
         nextSequence();
